f1e2207894b82771ea79c95c79dbdbf92c221a9d
Pauline Dao <paulined917@gmail.com>
2024-06-02 21:55:45 +0200

    created plotting module with 2 functions

---
+++ heart_disease/src/plotting.py
@@ -0,0 +1,34 @@
+"""
+    
+Module with plotting functions
+    
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+   
+def plot_pie_chart(df, column):
+    """_summary_
+    plot pie chart for categorical variables
+    Args:
+        df: dataframe
+        column (name of the column): type string
+    """
+    heart_disease = df[df['heart_disease_diagnosis'] == 1]
+    no_heart_disease = df[df['heart_disease_diagnosis'] == 0] 
+      
+    fig = plt.figure(figsize = (10,10))
+    fig.add_subplot(1,2,1)
+    heart_disease[column].value_counts().sort_index().plot(kind = 'pie', autopct = '%1.1f%%')
+    fig.add_subplot(1,2,2)
+    no_heart_disease[column].value_counts().sort_index().plot(kind = 'pie', autopct = '%1.1f%%')
+    plt.title(f'percentage of {column} in heart disease group (left) and healthy group (right)')
+    
+def contingency_table(data, column):
+    data_crosstab = pd.crosstab(index=[data[column]], columns=data['heart_disease_diagnosis'])#, margins = False) 
+    print(data_crosstab)
+    fig = plt.figure(figsize = (10,10))
+    fig.add_subplot()    
+    sns.heatmap(data_crosstab, annot = True, fmt="d", cmap = 'RdYlGn')
+    plt.show()
