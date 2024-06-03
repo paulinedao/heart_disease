e7f9d6b948c57a187afffc0ecf90367b7408143f
Pauline Dao <paulined917@gmail.com>
2024-06-03 12:58:59 +0200

    defined functions

--- heart_disease/src/load.py
+++ heart_disease/src/load.py
@@ -1,6 +1,39 @@
-    """
-    Module to load the data and display 5 first rows and 5 random rows
+"""
+Module to load the data 
     
-    """
+"""
 
 import pandas as pd
+
+def load_data(path):
+    """
+    load the data and creates the column names
+    Args:
+        path (string): _description_
+    """
+    df = pd.read_csv(path, header=None, na_values='?')
+    
+    df.columns = [
+    'age', 'sex', 'chest_pain', 'rest_blood_pressure', 'serum_cholesterol', 'fasting_blood_sugar', 'rest_ecg',
+    'max_heart_rate_thal', 'exercise_angina', 'st_oldpeak', 'st_slope', 'nb_major_vessels', 'thallium_stress_test', 'diagnosis'
+    ]
+    
+    return df
+
+def describe_data(df):
+    """_summary_
+
+    Args:
+        df (_type_): _description_
+    """
+
+    shape_of_data = df.shape
+    print(f'The number of (rows,columns) in this dataset is {shape_of_data}.')
+    print(' ')
+    
+    print('Information about columns names and types')
+    print(df.info())
+    
+    print(' ')
+    print('statistics summary of the dataset')
+    print(df.describe())
