d0b80e45241c3452d4bd399362fc4ec2934b129e
Pauline Dao <paulined917@gmail.com>
2024-06-05 17:43:50 +0200

    changede path to import package and data, changed df

--- heart_disease/test/test_plotting.py
+++ heart_disease/test/test_plotting.py
@@ -1,47 +1,47 @@
 """
 Module for testing
 """
+
 import sys
 import os
 import unittest
 
-
 package_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
 sys.path.insert(0, package_dir)
 
-from heart_disease.src.plotting import make_count_plots
-from heart_disease.src.plotting import hist_plot_numericals
-from heart_disease.src.plotting import plot_pie_chart
-from heart_disease.src.plotting import contingency_table
-from heart_disease.src.plotting import look_for_correlations
-from heart_disease.src.plotting import plot_all
+from src.process import process_all
+from src.plotting import make_count_plots
+from src.plotting import hist_plot_numericals
+from src.plotting import plot_pie_chart
+from src.plotting import contingency_table
+from src.plotting import look_for_correlations
+from src.plotting import plot_all
 
 class TestPlotting(unittest.TestCase):
     @classmethod
     # load the dataframe to use it as a test
     def setUp(cls): 
-        cls._df = load_data('../data/raw_data/processed.cleveland.data')
-        #cls._df = pd.read_csv('../data/raw_data/processed.cleveland.data')
+        cls._df = process_all('data/raw_data/processed.cleveland.data')
         
     def test_make_count_plots(self):
         # Check if the function executes without errors
-        make_count_plots(self.df)
+        make_count_plots(self._df)
 
     def test_hist_plot_numericals(self):
         # Check if the function executes without errors
-        hist_plot_numericals(self.df)
+        hist_plot_numericals(self._df)
         
     def test_plot_pie_chart(self):
         # Check if the function executes without errors
-        plot_pie_chart(self.df)
+        plot_pie_chart(self._df)
         
     def test_contingency_table(self):
         # Check if the function executes without errors
-        contingency_table(self.df)
+        contingency_table(self._df)
         
     def test_look_for_correlations(self):
         # Check if the function executes without errors
-        look_for_correlations(self.df)
+        look_for_correlations(self._df)
         
 if __name__ == '__main__':
     unittest.main()
