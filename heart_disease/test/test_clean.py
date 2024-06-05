ae331abe7796827948ca2395f4a4c71ad40a6ae5
Pauline Dao <paulined917@gmail.com>
2024-06-05 17:29:25 +0200

    test_clean OK!

--- heart_disease/test/test_clean.py
+++ heart_disease/test/test_clean.py
@@ -8,8 +8,11 @@ import unittest
 package_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, package_dir)
 
-from src.clean import impute_nan
+from src.load import load_data
+from src.process import process_all
 from src.clean import clean_all
+from src.clean import impute_nan
+
 
 
 
@@ -19,18 +22,19 @@ class TestClean(unittest.TestCase):
     # load the dataframe to use it as a test
     
     def setUp(cls): 
-        cls._df = load_data('../data/raw_data/processed.cleveland.data')
-        #cls._df = pd.read_csv('../data/raw_data/processed.cleveland.data')
-    
+
+        processed_df = process_all('data/raw_data/processed.cleveland.data')
+        cls._df = clean_all(processed_df)
+        
     def test_impute_nan(self):
         # test that the number of columns with null values is null
-       col_with_na =  df_clean.isna().any().sum()
+       col_with_na =  self._df.isna().any().sum()
        
        self.assertEqual(col_with_na, 0)
         
     def test_clean_all(self):
         # test that the number of duplicated and na values is null
-        duplicated_entries = df.duplicated().sum() 
+        duplicated_entries = self._df.duplicated().sum() 
         
         self.assertEqual(duplicated_entries, 0)
       
