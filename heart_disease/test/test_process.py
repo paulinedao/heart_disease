d0d8718292b260f8d4d73d3cab2415055e2a13a6
Pauline Dao <paulined917@gmail.com>
2024-06-05 18:17:50 +0200

    test OK, changed to seft.assertTrue to check data type

--- heart_disease/test/test_process.py
+++ heart_disease/test/test_process.py
@@ -11,29 +11,30 @@ sys.path.insert(0, package_dir)
 
 from src.process import process_categorical_columns
 from src.process import process_boolean_columns
-from src.process process_all
+from src.process import process_all
         
 class TestProcess(unittest.TestCase):
     @classmethod
     # load the dataframe to use it as a test
     def setUp(cls): 
-        cls._df = load_data('../data/raw_data/processed.cleveland.data')
-        #cls._df = pd.read_csv('../data/raw_data/processed.cleveland.data')
+        cls._df = process_all('data/raw_data/processed.cleveland.data')
 
         
     def test_process_categorical_columns(self):
-        processed_df = process_categorical_columns(self._df)
+        
         categorical_columns = ['sex', 'chest_pain','st_slope', 'thallium_stress_test', 'rest_ecg']
-        self.assertTrue(processed_df[categorical_columns].dtypes.equals(category))
-
+        for column in categorical_columns:
+            self.assertEqual(self._df.dtypes[column], 'category')
+   
         
     def test_process_boolean_columns(self):
-        processed_df = process_boolean_columns(self._df)
+        
         boolean_columns = ['high_fasting_blood_sugar', 'exercise_angina', 'heart_disease_diagnosis']
-        self.assertTrue(processed_df[categorical_columns].dtypes.equals(bool))
+        for column in boolean_columns:
+            self.assertEqual(self._df.dtypes[column], 'bool')
 
     def test_process_all(self):
-        self.assertTrue(os.path.exists('data/processed_data/processed_data'))
+        self.assertTrue(os.path.exists('data/processed_data/processed_data.csv'))
         
 
 
