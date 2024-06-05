ff889169111fcf15e968f2253628291b48f7a03c
Pauline Dao <paulined917@gmail.com>
2024-06-05 17:53:12 +0200

    changed path to import and self instead of cls

--- heart_disease/test/test_process.py
+++ heart_disease/test/test_process.py
@@ -9,9 +9,9 @@ import unittest
 package_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
 sys.path.insert(0, package_dir)
 
-from heart_disease.src.process import process_categorical_columns
-from heart_disease.src.process import process_boolean_columns
-from heart_disease.src.process process_all
+from src.process import process_categorical_columns
+from src.process import process_boolean_columns
+from src.process process_all
         
 class TestProcess(unittest.TestCase):
     @classmethod
@@ -22,13 +22,13 @@ class TestProcess(unittest.TestCase):
 
         
     def test_process_categorical_columns(self):
-        processed_df = process_categorical_columns(cls._df)
+        processed_df = process_categorical_columns(self._df)
         categorical_columns = ['sex', 'chest_pain','st_slope', 'thallium_stress_test', 'rest_ecg']
         self.assertTrue(processed_df[categorical_columns].dtypes.equals(category))
 
         
     def test_process_boolean_columns(self):
-        processed_df = process_boolean_columns(cls._df)
+        processed_df = process_boolean_columns(self._df)
         boolean_columns = ['high_fasting_blood_sugar', 'exercise_angina', 'heart_disease_diagnosis']
         self.assertTrue(processed_df[categorical_columns].dtypes.equals(bool))
 
